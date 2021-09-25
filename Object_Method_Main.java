61d36f2cddb3f88e759d7294c5c8d1f69746e835
lalithakadiyala <34548493+lalithakadiyala@users.noreply.github.com>
2021-09-25 13:52:00 -0700

    Add files via upload

---
+++ Object_Method_Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Object_Method_Main  {
+    public static void main(String[] args) {
+        Object_Method om = new Object_Method(87, "Karunya");
+        om.setManager("Hassir");
+        om.getManager();
+        //System.out.println(     om.getManager());
+        om.sal(2700);
+
+    }
+
+
+
+}
